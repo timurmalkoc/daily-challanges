81e96986cffd232d05be0f579826e3f541a5ca34
timurmalkoc <malkoctimurcelik@gmail.com>
2022-09-29 22:18:43 -0400

    alternatif solution

--- findmax.py
+++ findmax.py
@@ -14,3 +14,17 @@ def find_max(lst):
 print(find_max([-2,1,-3,4,-1,2,1,-5,4]))
 print(find_max([1]))
 print(find_max([5,4,-1,7,8]))
+
+def find_max2(lst):
+    mx = lst[0]
+    for pointer in range(1,len(lst)):
+        if mx > pointer:
+            mx += pointer
+        else:
+            mx = pointer
+            
+    return mx
+
+print(find_max([-2,1,-3,4,-1,2,1,-5,4]))
+print(find_max([1]))
+print(find_max([5,4,-1,7,8]))
