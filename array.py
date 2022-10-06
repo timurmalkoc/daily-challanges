fb93b95c68d933a67056f5f0655e5389b1d6a9aa
timurmalkoc <malkoctimurcelik@gmail.com>
2022-10-06 10:37:24 -0400

    find non-repeat

--- array.py
+++ array.py
@@ -3,3 +3,22 @@ def add_one(lst):
 
 
 print(add_one([4,3,2,1]))
+
+
+def find_non_repeat(st):
+    d = {}
+    for i in st:
+        if i in d:
+            d[i] += 1 
+        else:
+            d[i] = 1
+
+    for index, val in enumerate(st):
+        if d[val] == 1:
+            return index
+    else:
+        return -1
+
+print(find_non_repeat("leetcode"))
+print(find_non_repeat("loveleetcode"))
+print(find_non_repeat("aabb"))
