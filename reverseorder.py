125e32aecb51b48a2bae157b2c944ba8ea54cb6c
timurmalkoc <malkoctimurcelik@gmail.com>
2022-11-11 15:49:10 -0500

    longest palindrome

--- reverseorder.py
+++ reverseorder.py
@@ -3,3 +3,16 @@ def reverse(st):
 
 print(reverse("Let's take LeetCode contest"))
 print(reverse("God Ding"))
+
+
+def longestPalindrome(s):
+        pal = ''
+        for i in range(len(s)-1):
+            for j in range(i+1, len(s)):
+                if s[i:j] == s[i:j][::-1] and len(pal)<j-i:
+                    pal = s[i:j]
+        return pal
+
+
+print(longestPalindrome("babb"))
+print(longestPalindrome("abcd"))
