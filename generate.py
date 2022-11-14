76ec56154c860809cadd35a63ff793d1f550b164
timurmalkoc <malkoctimurcelik@gmail.com>
2022-11-14 16:42:41 -0500

    generate Parentheses

---
+++ generate.py
@@ -0,0 +1,14 @@
+def generate(n):
+    res = []
+    s = [("(", 1, 0)]
+    while s:
+        x, l, r = s.pop()
+        if l - r < 0 or l > n or r > n:
+            continue
+        if l == n and r == n:
+            res.append(x)
+        s.append((x+"(", l+1, r))
+        s.append((x+")", l, r+1))
+    return res
+
+print(generate(3))
