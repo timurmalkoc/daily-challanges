8e40ed8b6abc2a8d87907547a6ab0132b9a4f1bb
timurmalkoc <malkoctimurcelik@gmail.com>
2022-11-22 11:41:40 -0500

    largest kth element

--- sorting.py
+++ sorting.py
@@ -9,3 +9,10 @@ def sortColors(nums):
 
 print(sortColors([2,0,2,1,1,0]))
 print(sortColors([2,0,1]))
+
+
+def findKthLargest(nums, k):
+    return sorted(nums, reverse=True)[k-1]
+
+print(findKthLargest([3,2,1,5,6,4], 2))
+print(findKthLargest([3,2,3,1,2,4,5,5,6], 1))
