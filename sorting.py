dbdb4c872450ff59bb120873b949126b0b3f01fd
timurmalkoc <malkoctimurcelik@gmail.com>
2022-11-23 10:36:10 -0500

    merge intervals

--- sorting.py
+++ sorting.py
@@ -44,3 +44,14 @@ def containsDuplicate(nums):
 
 print(containsDuplicate([1,2,3,1]))
 print(containsDuplicate([1,2,3,4]))
+
+def merge(intervals):
+    out = []
+    for i in sorted(intervals, key=lambda i: i[0]):
+        if out and i[0] <= out[-1][1]:
+            out[-1][1] = max(out[-1][1], i[1])
+        else:
+            out += i,
+    return out
+
+print(merge([[1,3],[2,6],[8,10],[15,18]]))
