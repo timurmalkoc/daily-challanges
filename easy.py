575c05d63d24ba8f6b3fe70aec2c12b7ac7f8559
timurmalkoc <malkoctimurcelik@gmail.com>
2022-11-29 19:10:12 -0500

    anagram

--- easy.py
+++ easy.py
@@ -9,3 +9,9 @@ def majorityElement(nums):
 
 print(majorityElement([2,2,1,1,1,2,2]))
 print(majorityElement([3,2,3]))
+
+def isAnagram(s, t):
+    return True if sorted(list(s)) == sorted(list(t)) else False
+
+print(isAnagram("anagram", "nagaram"))
+print(isAnagram("rat", "car"))
