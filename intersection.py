39d1268359916d6b938f7430ea63d00881ce996b
timurmalkoc <malkoctimurcelik@gmail.com>
2022-12-01 19:19:15 -0500

    missing number

--- intersection.py
+++ intersection.py
@@ -3,3 +3,13 @@ def intersection(nums1, nums2):
 
 print(intersection([1,2,2,1], [2, 2]))
 print(intersection([4,9,5], [9,4,9,8,4]))
+
+def missingNumber(nums):
+    nums = sorted(nums)
+    for x in range(0,len(nums)):
+        if x != nums[x]:
+            return x
+    return nums[-1]+1
+
+print(missingNumber([1, 2, 3]))
+print(missingNumber([0, 3, 1, 4]))
