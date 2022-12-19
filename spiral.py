57f4d5ceff15e31d71f80e2796af5b0fe48e055f
timurmalkoc <malkoctimurcelik@gmail.com>
2022-12-19 18:27:41 -0500

    spiral

---
+++ spiral.py
@@ -0,0 +1,14 @@
+def generateMatrix(n):
+    A = [[0] * n for _ in range(n)]
+    i, j, di, dj = 0, 0, 0, 1
+    for k in range(n*n):
+        A[i][j] = k + 1
+        if A[(i+di)%n][(j+dj)%n]:
+            di, dj = dj, -di
+        i += di
+        j += dj
+    return A
+
+print(generateMatrix(9))
+print(generateMatrix(1))
+print(generateMatrix(3))
