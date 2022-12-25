b2a2aef4aca84d58c27d49b0a17f95d4ec95080d
timurmalkoc <malkoctimurcelik@gmail.com>
2022-12-25 17:00:31 -0500

    binary tree

---
+++ BinarySearc.py
@@ -0,0 +1,17 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def sortedArrayToBST(self, num):
+        if not num:
+            return None
+
+        mid = len(num) // 2
+
+        root = TreeNode(num[mid])
+        root.left = self.sortedArrayToBST(num[:mid])
+        root.right = self.sortedArrayToBST(num[mid+1:])
+
+        return root
